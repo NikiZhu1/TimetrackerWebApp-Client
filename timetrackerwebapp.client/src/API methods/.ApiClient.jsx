998390aa57d9b3bf74ca301554762e9221d77389
nikita <nikita@DESKTOP-L52QOPR>
2025-05-19 20:27:32 +0500

    action

--- timetrackerwebapp.client/src/API methods/.ApiClient.jsx
+++ timetrackerwebapp.client/src/API methods/.ApiClient.jsx
@@ -1,11 +1,9 @@
 import axios from 'axios';
 
-// const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
-
-// if (!process.env.REACT_APP_API_BASE_URL)
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL ?? 'http://localhost:8080/api';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: API_BASE_URL,
   timeout: 10000,
   headers: { 'Content-Type': 'application/json' }
 });
